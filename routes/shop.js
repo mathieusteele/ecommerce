ea4cc4378f8d7f30092622b8d3a810e0013057a1
Mathieu Steele <matt@steeleagency.com>
2021-09-28 03:15:12 -0400

    remove items from cart

--- routes/shop.js
+++ routes/shop.js
@@ -8,18 +8,12 @@ const router = express.Router();
 router.get("/", shopController.getIndex);
 
 router.post("/add-to-cart", shopController.postAddToCart);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.get("/cart", shopController.getCart);
 router.get("/checkout", shopController.getCheckout);
 router.get("/orders", shopController.getOrders);
 router.get("/products", shopController.getProducts);
 
 router.get("/products/:productId", shopController.getProductById);
-
-// router.get("/", (req, res, next) => {
-//   res.render("index", {
-//     title: "Home",
-//     path: "/",
-//   });
-// });
 
 module.exports = router;
