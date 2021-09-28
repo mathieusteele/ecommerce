33b474963e844f9415d3dd141cf75ab91a537289
Mathieu Steele <matt@steeleagency.com>
2021-09-28 04:26:40 -0400

    remove log

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,7 +15,7 @@ exports.getProductById = (req, res, next) => {
   const productId = req.params.productId;
 
   Product.findById(productId, (product) => {
-    console.log(product);
+    // console.log(product);
 
     res.render("shop/product-detail", {
       title: "Product Details",
