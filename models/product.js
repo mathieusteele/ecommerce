8b223b26b587d0c5f3034171ef47d93fa93de4f2
Mathieu Steele <matt@steeleagency.com>
2021-09-28 03:14:59 -0400

    cart functionality

--- models/product.js
+++ models/product.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const rootDir = require("../util/path");
 
+const Cart = require("./cart");
+
 const p = path.join(rootDir, "data", "products.json");
 
 const getProductsFromFile = (callback) => {
-  //   const p = path.join(rootDir, "data", "products.json");
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       callback([]);
@@ -16,29 +17,49 @@ const getProductsFromFile = (callback) => {
 };
 
 module.exports = class Product {
-  constructor(title, author, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
-    this.author = author;
     this.imageUrl = imageUrl;
     this.description = description;
     this.price = price;
   }
 
   save() {
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
-    // const p = path.join(rootDir, "data", "products.json");
-    // fs.readFile(p, (err, fileContent) => {
-    //   let products = [];
-    //   if (!err) {
-    //     products = JSON.parse(fileContent);
-    //   }
-    // });
+  }
+
+  static deleteById(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => prod.id === id);
+      const updatedProducts = products.filter((product) => product.id !== id);
+      fs.writeFile(
+        p,
+        JSON.stringify(updatedProducts, (err) => {
+          if (!err) {
+            Cart.deleteProduct(id, product.price);
+          }
+        })
+      );
+      callback(theProduct);
+    });
   }
 
   static fetchAll(callback) {
