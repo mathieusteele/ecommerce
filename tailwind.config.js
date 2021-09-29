44616c8ea16081ed2c956c69ee5391ef11d558ec
Mathieu Steele <matt@steeleagency.com>
2021-09-28 22:20:59 -0400

    add tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["../**/*.ejs", "../**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
