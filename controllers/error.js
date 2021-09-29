a9c6aa3e0f6b9ac1f07b077280a508ce2f7f50a3
Mathieu Steele <matt@steeleagency.com>
2021-09-28 21:46:33 -0400

    enhance the 404 page

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,7 @@
 exports.get404 = (req, res, next) => {
-  res.render("404", { title: "404 - Page Not Found", path: "/404" });
+  res.render("404", {
+    title: "404 - Page Not Found",
+    path: "/404",
+    requestedPath: req.url,
+  });
 };
