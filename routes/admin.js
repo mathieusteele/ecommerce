0fc307b80cea7b8539bb25f0c02f77b00fc6e521
Mathieu Steele <matt@steeleagency.com>
2021-10-22 23:25:29 -0400

    add validation messages for entree add form

--- routes/admin.js
+++ routes/admin.js
@@ -12,10 +12,23 @@ router.get("/add-entree", isAuth, adminController.getAddProduct);
 router.post(
   "/add-entree",
   [
-    body("title").isString().isLength({ min: 3 }).trim(),
-    body("imageUrl").isURL(),
-    body("price").isFloat(),
-    body("description").isLength({ min: 5, max: 400 }).trim(),
+    body("title")
+      .isString()
+      .isLength({ min: 3 })
+      .trim()
+      .withMessage("the title must be at least 3 characters long"),
+    body("imageUrl")
+      .isURL()
+      .withMessage(
+        "The image URL is a required field and did not contain a valid URL."
+      ),
+    body("price")
+      .isFloat()
+      .withMessage("The price was not valid. Please try again."),
+    body("description")
+      .isLength({ min: 5, max: 400 })
+      .trim()
+      .withMessage("The description must be between 5 and 400 characters."),
   ],
   isAuth,
   adminController.postAddProduct
